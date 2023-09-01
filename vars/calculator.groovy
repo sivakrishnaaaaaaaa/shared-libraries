4768714a15d445de56ca283c6dd5a6ee1c2aab80
sivakrishnaaaaaaaa <142010173+sivakrishnaaaaaaaa@users.noreply.github.com>
2023-09-01 07:16:54 +0530

    Update calculator.groovy

--- vars/calculator.groovy
+++ vars/calculator.groovy
@@ -2,7 +2,7 @@ def add(i,j) {
   result =i+j
   echo "sum of ${i} and ${j} is ${result}"
 }
-def multiply (i,j) { 
+def multiply(i,j) { 
   result =i*j
   echo "multiplication of ${i} and ${j} is ${result}"
 }
